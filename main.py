1b31112131e5f461bf66c2a1b8a73e995d0cd9a4
NikonPic <nikolas@wilhelm.nu>
2023-01-05 11:24:06 +0100

    Ganz tolle Änderungen

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 # packages
 from argmax_gym import TcpRemoteGym
 import numpy as np
+import json
 from datasplit import ObservationHandler
 from definitions import action_off, action_amp, action_phase
 import cv2 as cv
+import time
 from datetime import datetime
 
 # def init
-video_path = '/home/robotlab/Documents/GitHub/MA_Schote/MA/Data/Video'
+safe_path = '/home/robotlab/Documents/GitHub/MA_Schote/MA/Data'
 
 json_out = {
     'time': [],
@@ -17,7 +19,7 @@ json_out = {
 
 obs_handler = ObservationHandler(num_rigid_bodies=0)
 
-
+print('Conntecting to TcpRemote.')
 env = TcpRemoteGym(
     "phoenix-v0",
     hosts=["141.39.191.228"],
@@ -26,7 +28,7 @@ env = TcpRemoteGym(
 )
 
 print('init')
-#obs, image = env.reset(record=False)
+obs, image = env.reset(record=False)
 print('reset')
 
 start_time = datetime.now()
@@ -54,9 +56,9 @@ def videocapture():
     
     return frame
 
-def millis(init_time):
+def millis(init_time, now):
     '''Vergangene Zeit seit init_time in ms'''
-    now = datetime.now() # Format: datetime.datetime(2023, 1, 4, 14, 20, 12, 623357)
+    #now = datetime.now() # Format: datetime.datetime(2022, 12, 4, 14, 20, 12, 623357)
     dt = now - init_time # Format: datetime.timedelta(seconds=1, microseconds=1470)
     dt_in_ms = dt.seconds *1000 + dt.microseconds / 1000
     return dt_in_ms
@@ -64,7 +66,7 @@ def millis(init_time):
 #%% main
 if __name__ == '__main__':
     while t <= t_max:
-        t = millis(start_time)
+        t = millis(start_time, datetime.now())
         
         frame = videocapture()
         video_list.append(frame)
@@ -83,13 +85,26 @@ if __name__ == '__main__':
     
 
     # Close Test
-    # release camera
-    cap.release()
-    cv.destroyAllWindows()
+    print('Finished Testrun.')
+    # dump json
+    json_out['observation'] = obs_handler.output_dict
+    tt = time.localtime()
+    current_time = time.strftime("%H_%M_%S", tt)
+
+    with open(f'{safe_path}/{current_time}.json', 'w') as f:
+        json.dump(json_out, f, indent=2)
+
     #save video
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
-    out = cv.VideoWriter(f'{video_path}/{start_time}.avi',cv.VideoWriter_fourcc(*'DIVX'), frameSize=(frame_width,frame_height), fps=25)
+    out = cv.VideoWriter(f'{safe_path}/Video/{current_time}.avi',cv.VideoWriter_fourcc(*'DIVX'), frameSize=(frame_width,frame_height), fps=25)
     for i in range(len(video_list)):
-        frame = video_list[i][:size, :size]
+        frame = video_list[i]
         out.write(frame)
+    # release camera
+    cap.release()
+    cv.destroyAllWindows()
+
+    f.close()
+
+    print('Data saved and closed!')
