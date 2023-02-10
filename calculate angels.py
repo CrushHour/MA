f3852949ffc21d498e1cbaf2a8ddb9bbf1e69d95
CrushHour <julian.schote@tum.de>
2023-02-10 17:19:58 +0100

    asd

--- calculate angels.py
+++ calculate angels.py
@@ -8,4 +8,8 @@ opti_data = 'Take 2023-01-31 06.11.42 PM.csv'
 
 ZF_DIP_raw = trackers.read_markerdata(os.path.join(data_path,opti_data), '55', True)
 Tracker_55 = trackers.Tracker(0, './Data/Trackers/DAU_DIP.csv')
+
+ZF_DIP_ct = Tracker_55.t_ct_def * ZF_DIP_raw
 #%%
+# test_points1 = [[2,-5,4], [5,6,7], [-10,0,3], [-3,11,13], [8,5,4]]
+# test_points2 = [[-1,4,3], [8,4,-3], [12,7,9], [4,-5,6], [-2,10,7]]
